bcee7585b315a7e487f3ad4736dcd1b1e4365d43
Lena <svintusiatko@gmail.com>
2016-03-21 15:01:54 +0200

    Revert "Revert "First release""
    This reverts commit 4afa9101f1c6010e9b4f3ad26c26e21232ac074e.

---
+++ PerfectMoneyAutoSender/Cryptor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PerfectMoneyAutoSender
+{
+    class Cryptor
+    {
+        public static string Encrypt(string toEncrypt, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+
+            AppSettingsReader settingsReader = new AppSettingsReader();
+
+            string key = "d5l;hyhGjvj8LJKBdfhvkHHC";
+
+            if (useHashing)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(key));
+
+                hashmd5.Clear();
+            }
+            else
+                keyArray = Encoding.UTF8.GetBytes(key);
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            tdes.Clear();
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        public static string Decrypt(string cipherString, bool useHashing)
+        {
+            byte[] keyArray;
+
+            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+
+            System.Configuration.AppSettingsReader settingsReader = new AppSettingsReader();
+
+            string key = "d5l;hyhGjvj8LJKBdfhvkHHC";
+
+            if (useHashing)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(key));
+
+                hashmd5.Clear();
+            }
+            else
+            {
+                keyArray = Encoding.UTF8.GetBytes(key);
+            }
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tdes.Clear();
+
+            return Encoding.UTF8.GetString(resultArray);
+        }
+    } 
+}
